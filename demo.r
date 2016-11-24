2ea8b9820c9b683c8bf3a0ccbd46b189bd595faa
udhanishreya <shreya190994@gmail.com>
2016-11-24 09:47:52 +0530

    Add files via upload

---
+++ demo.r
@@ -0,0 +1,5 @@
+x<-data.frame(
+names=c("a","b","c"),
+class=c(1,2,3),
+salary=c(10,15,20),
+age=c(7,8,9))
---
+++ new.r
@@ -0,0 +1,4 @@
+structure(list(names = structure(1:3, .Label = c("a", "b", "c"
+), class = "factor"), class = c(1, 2, 3), salary = c(10, 15, 
+20), age = c(7, 8, 9)), .Names = c("names", "class", "salary", 
+"age"), row.names = c(NA, -3L), class = "data.frame")
